8337fea6de678f25816fa5dce1ba788190669c79
patrykkoziel <patrykkoziel99@o2.pl>
2021-02-24 20:16:33 +0100

    identityAndApiConnect

---
+++ DotNetProject/Models/JobModel.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetProject.Models
+{
+    public class JobModel
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        
+        [JsonProperty("title")]
+
+        public string Title { get; set; }
+
+        [JsonProperty("category")]
+        public string Category { get; set; }
+        
+        [JsonProperty("company_name")]
+        public string Company { get; set; }
+        
+        [JsonProperty("url")]
+        public string Url { get; set; }
+        
+        [JsonProperty("publication_date")]
+        public string PublicationDate { get; set; }
+    }
+}
